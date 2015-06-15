4482dc03492fc5f79e2198b52202ec7853a8d0f2
jasony62 <jasony62@qq.com>
2015-06-15 09:04:35 +0800

    铁建地产初始版本

---
+++ cus/db.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * 数据库连接参数
+ */
+define('TMS_MYSQL_HOST', 'localhost');
+define('TMS_MYSQL_PORT', '3306');
+define('TMS_MYSQL_USER', 'weixin');
+define('TMS_MYSQL_PASS', 'wxcs123');
+define('TMS_MYSQL_DB', 'weixin');
