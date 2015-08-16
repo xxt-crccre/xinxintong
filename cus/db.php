83d67ce241b54891f7cc3d36afacb8a79247b131
jasony62 <jasony62@qq.com>
2015-08-16 22:17:57 +0800

    忽略根目录的cus文件夹

--- cus/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * 数据库连接参数
- */
-define('TMS_MYSQL_HOST', 'localhost');
-define('TMS_MYSQL_PORT', '3306');
-define('TMS_MYSQL_USER', 'weixin');
-define('TMS_MYSQL_PASS', 'wxcs123');
-define('TMS_MYSQL_DB', 'weixin');
